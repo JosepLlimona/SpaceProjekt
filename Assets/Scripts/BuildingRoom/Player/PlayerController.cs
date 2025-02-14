2b86e5b5e85159e8fd71098b4d17ea94f23d8316
JosepLlimona <62258739+JosepLlimona@users.noreply.github.com>
2025-02-14 14:50:38 +0100

    Movement 1

--- Assets/Scripts/BuildingRoom/Player/PlayerController.cs
+++ Assets/Scripts/BuildingRoom/Player/PlayerController.cs
@@ -9,8 +9,14 @@ public class PlayerController : MonoBehaviour
     [Header("Movment")]
     [SerializeField] float speed = 5f;
     [SerializeField] bool canRun = true;
+    [SerializeField] float dashingPower = 24f;
+    [SerializeField] float dashingTime = 0.2f;
+    [SerializeField] float dashingCooldown = 1f;
     Rigidbody2D rb;
     Vector2 dir = Vector2.zero;
+    bool isRunning = false;
+    bool canDash = true;
+    bool isDashing = false;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +26,7 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if (isDashing) { return; }
         rb.velocity = dir * speed;
     }
 
@@ -33,16 +40,49 @@ public class PlayerController : MonoBehaviour
     {
         if (canRun)
         {
-            if (context.started)
+            if (context.performed)
             {
-                Debug.Log("Started");
+                Debug.Log("Started run");
                 speed = 10f;
+                isRunning = true;
             }
             if (context.canceled)
             {
-                Debug.Log("Canceled");
-                speed = 5f;
+                if (isRunning)
+                {
+                    Debug.Log("Canceled run");
+                    speed = 5f;
+                    isRunning = false;
+                }
             }
         }
+    }
+
+    public void Dash(InputAction.CallbackContext context)
+    {
+        if (context.performed && !isRunning && canDash)
+        {
+            Debug.Log("Performed dash");
+            StartCoroutine(Dash());
+        }
+    }
+
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+
+        if (dir != Vector2.zero)
+        {
+            rb.velocity = dir * dashingPower;
+        }
+        else
+        {
+            rb.velocity = Vector2.up * dashingPower;
+        }
+        yield return new WaitForSeconds(dashingTime);
+        isDashing = false;
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
     }
 }
